1f9862596d69f5cfa2be2436a37ffd63c9dbfa3d
Manav Chaturvedi <mcvedi007@gmail.com>
2022-04-22 18:05:24 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,38 @@
+// #include <bits/stdc++.h>
+
+// using namespace std;
+
+//  SWAPPING VIA POINTERS
+
+// int swap(int *x, int *y){
+//     int temp;
+//     temp= *x;
+//     *x = *y;
+//     *y = temp;
+// }
+
+// int main() {
+//     int a,b;
+//     cin>>a>>b;
+//     swap(&a,&b);
+//     cout<<a<<" "<<b;
+//     return 0;
+// }
+
+
+//  SWAPPING VIA REFERENCE
+
+// int swap(int &x, int &y){
+//     int temp;
+//     temp = x;
+//     x=y;
+//     y=temp;
+// }
+
+// int main(){
+//     int a,b;
+//     cin>>a>>b;
+//     swap(a,b);;
+//     cout<<a<<" "<<b;
+//     return 0;
+// }
